5ce5ac510ad178b3046e00e34e587a94f6f9bdfa
Nilesh_office <nileshwephyre@gmail.com>
2021-01-08 16:51:21 +0530

    changed db password

--- application/views/admin/layout/header.php
+++ application/views/admin/layout/header.php
@@ -4,14 +4,14 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title><?php echo isset($header)?$header:''; ?></title>
+    <title><?php echo isset($header) ? $header : ''; ?></title>
 
     <!-- Google Font: Source Sans Pro -->
     <!-- <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback"> -->
 
-     <!--Load the AJAX API-->
-     <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-     <script src="https://code.highcharts.com/highcharts.js"></script>
+    <!--Load the AJAX API-->
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script src="https://code.highcharts.com/highcharts.js"></script>
     <script src="https://code.highcharts.com/modules/data.js"></script>
     <script src="https://code.highcharts.com/modules/drilldown.js"></script>
     <script src="https://code.highcharts.com/modules/exporting.js"></script>
@@ -45,14 +45,15 @@
     <link rel="stylesheet" href="<?php echo base_url() ?>assets/plugins/toastr/toastr.min.css">
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/css/bootstrap-datepicker.min.css" />
-    
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery-timepicker/1.10.0/jquery.timepicker.min.css"/>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery-timepicker/1.10.0/jquery.timepicker.min.css" />
     <script>
         const BASEURL = '<?php echo base_url() ?>';
     </script>
     <!-- Theme style -->
     <link rel="stylesheet" href="<?php echo base_url('assets/') ?>dist/css/adminlte.min.css">
 </head>
+
 <body class="hold-transition sidebar-mini layout-fixed layout-navbar-fixed layout-footer-fixed">
     <div class="wrapper">
         <!-- Navbar -->
@@ -65,7 +66,13 @@
             </ul>
             <!-- Right navbar links -->
             <ul class="navbar-nav ml-auto">
-    
+
+
+                <li class="nav-item">
+                    <a class="nav-link text-primary fs-23" href="<?php echo base_url('profile/').base64_encode($_SESSION['logged_in']['people_id'])?>" role="button" title="Change password">
+                    <i class="fas fa-cogs"></i>
+                    </a>
+                </li>
                 <li class="nav-item">
                     <a class="nav-link text-red fs-23" href="<?php echo base_url('Login/logout') ?>" role="button" title="Logout">
                         <i class="fas fa-sign-out-alt"></i>
